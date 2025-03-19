6d95d22bc9322613b940670ecf7c35173a201bcf
Jovakinn <max05012004@gmail.com>
2025-03-19 23:39:18 +0200

    add MPI Gif generation

--- mpi.c
+++ mpi.c
@@ -3,6 +3,8 @@
 #include <math.h>
 #include <mpi.h>
 
+#define NUM_FRAMES 10  // Кількість кадрів для анімації
+
 int main(int argc, char *argv[]) {
     int rank, size;
     int WIDTH, HEIGHT, MAX_ITER;
@@ -43,139 +45,189 @@ int main(int argc, char *argv[]) {
     MPI_Bcast(&imag_max, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     // Після цих викликів, усі процеси мають однакові значення параметрів.
 
-    // Розподіл рядків зображення між процесами.
-    // Кожен процес отримає певну кількість рядків для обчислення.
-    int rows_per_proc = HEIGHT / size;
-    int remainder = HEIGHT % size;
-    int start_row, end_row;
-    // Якщо rank менший за залишок, процесу виділяється додатковий рядок.
-    if (rank < remainder) {
-        start_row = rank * (rows_per_proc + 1);
-        end_row = start_row + rows_per_proc + 1;
-    } else {
-        start_row = rank * rows_per_proc + remainder;
-        end_row = start_row + rows_per_proc;
-    }
-    int local_rows = end_row - start_row;
+    // NEW: Додаткові параметри для динамічної візуалізації (zoom‑ефект)
+    double center_real = -0.5, center_imag = 0.0;  // Центр збільшення
+    double initial_width = real_max - real_min;      // Початкова ширина області
+    double initial_height = imag_max - imag_min;       // Початкова висота області
+    double zoom_step = 1.05;                         // NEW: Крок збільшення для кожного кадру
 
-    // Виділення пам'яті для локального блоку зображення.
-    // Кожен процес створює масив, в якому зберігатиме результати обчислень (кількість ітерацій) для своїх рядків.
-    int *local_image = malloc(local_rows * WIDTH * sizeof(int));
+    // Цикл для генерації NUM_FRAMES кадрів анімації
+    for (int frame = 0; frame < NUM_FRAMES; frame++) {
 
-    // Локальні статистичні змінні для накопичення результатів:
-    // - local_total: сума ітерацій для локального блоку;
-    // - local_min: мінімальна кількість ітерацій серед пікселів локального блоку;
-    // - local_max: максимальна кількість ітерацій.
-    long long local_total = 0;
-    int local_min = MAX_ITER;
-    int local_max = 0;
+        // NEW: Обчислення динамічних параметрів для поточного кадру (zoom‑ефект)
+        double zoom = pow(zoom_step, frame);
+        double current_width = initial_width / zoom;
+        double current_height = initial_height / zoom;
+        // Оновлення меж області для побудови множини Мандельброта для поточного кадру
+        real_min = center_real - current_width / 2;
+        real_max = center_real + current_width / 2;
+        imag_min = center_imag - current_height / 2;
+        imag_max = center_imag + current_height / 2;
 
-    // Фіксуємо час початку локальних обчислень.
-    // MPI_Wtime повертає поточний час, що дозволяє нам обчислити час виконання.
-    double start_time = MPI_Wtime();
-
-    // Обчислення локального блоку (рядків) множини Мандельброта.
-    // Кожен процес обчислює свій набір пікселів, що відповідають його діапазону рядків.
-    for (int i = 0; i < local_rows; i++) {
-        int global_row = start_row + i;
-        // Для кожного 500-го глобального рядка виводимо інформацію про хід обчислень.
-        if (global_row % 500 == 0) {
-            printf("Процес %d: обробка глобального рядка %d\n", rank, global_row);
+        // Розподіл рядків зображення між процесами.
+        // Кожен процес отримає певну кількість рядків для обчислення.
+        int rows_per_proc = HEIGHT / size;
+        int remainder = HEIGHT % size;
+        int start_row, end_row;
+        // Якщо rank менший за залишок, процесу виділяється додатковий рядок.
+        if (rank < remainder) {
+            start_row = rank * (rows_per_proc + 1);
+            end_row = start_row + rows_per_proc + 1;
+        } else {
+            start_row = rank * rows_per_proc + remainder;
+            end_row = start_row + rows_per_proc;
         }
-        double imag = imag_max - global_row * (imag_max - imag_min) / (HEIGHT - 1);
-        for (int j = 0; j < WIDTH; j++) {
-            double real = real_min + j * (real_max - real_min) / (WIDTH - 1);
-            double z_real = 0.0, z_imag = 0.0;
-            int iter = 0;
-            // Основний цикл обчислення для визначення, чи належить точка множині Мандельброта.
-            while (z_real * z_real + z_imag * z_imag <= 4.0 && iter < MAX_ITER) {
-                double temp = z_real * z_real - z_imag * z_imag + real;
-                z_imag = 2.0 * z_real * z_imag + imag;
-                z_real = temp;
-                iter++;
+        int local_rows = end_row - start_row;
+
+        // Виділення пам'яті для локального блоку зображення.
+        // Кожен процес створює масив, в якому зберігатиме результати обчислень (кількість ітерацій) для своїх рядків.
+        int *local_image = malloc(local_rows * WIDTH * sizeof(int));
+
+        // Локальні статистичні змінні для накопичення результатів:
+        // - local_total: сума ітерацій для локального блоку;
+        // - local_min: мінімальна кількість ітерацій серед пікселів локального блоку;
+        // - local_max: максимальна кількість ітерацій.
+        long long local_total = 0;
+        int local_min = MAX_ITER;
+        int local_max = 0;
+
+        // Фіксуємо час початку локальних обчислень.
+        // MPI_Wtime повертає поточний час, що дозволяє нам обчислити час виконання.
+        double start_time = MPI_Wtime();
+
+        // Обчислення локального блоку (рядків) множини Мандельброта.
+        // Кожен процес обчислює свій набір пікселів, що відповідають його діапазону рядків.
+        for (int i = 0; i < local_rows; i++) {
+            int global_row = start_row + i;
+            // Для кожного 500-го глобального рядка виводимо інформацію про хід обчислень.
+            if (global_row % 500 == 0) {
+                printf("Процес %d: обробка глобального рядка %d (Frame %d)\n", rank, global_row, frame);
             }
-            // Збереження результату для пікселя.
-            local_image[i * WIDTH + j] = iter;
-            local_total += iter;
-            if (iter < local_min)
-                local_min = iter;
-            if (iter > local_max)
-                local_max = iter;
+            double imag_val = imag_max - global_row * (imag_max - imag_min) / (HEIGHT - 1);
+            for (int j = 0; j < WIDTH; j++) {
+                double real_val = real_min + j * (real_max - real_min) / (WIDTH - 1);
+                double z_real = 0.0, z_imag = 0.0;
+                int iter = 0;
+                // Основний цикл обчислення для визначення, чи належить точка множині Мандельброта.
+                while (z_real * z_real + z_imag * z_imag <= 4.0 && iter < MAX_ITER) {
+                    double temp = z_real * z_real - z_imag * z_imag + real_val;
+                    z_imag = 2.0 * z_real * z_imag + imag_val;
+                    z_real = temp;
+                    iter++;
+                }
+                // Збереження результату для пікселя.
+                local_image[i * WIDTH + j] = iter;
+                local_total += iter;
+                if (iter < local_min)
+                    local_min = iter;
+                if (iter > local_max)
+                    local_max = iter;
+            }
+        }
+
+        long long global_total;
+        int global_min, global_max;
+        double global_time;
+
+        // Збір глобальної статистики за допомогою MPI_Reduce.
+        // MPI_Reduce виконує операцію редукції (сума, мінімум, максимум) над значеннями з усіх процесів.
+        // Результат операції надсилається до процесу, заданого як root (тут 0).
+        MPI_Reduce(&local_total, &global_total, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+        MPI_Reduce(&local_min, &global_min, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+        MPI_Reduce(&local_max, &global_max, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+
+        // Фіксуємо час завершення обчислень разом з кінцем передачі усіх даних на 0 процес
+        double end_time = MPI_Wtime();
+        double local_time = end_time - start_time;
+        MPI_Reduce(&local_time, &global_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+        // Підготовка до збору глобального зображення.
+        // Процес 0 (root) виділяє пам'ять для отримання результатів з усіх процесів.
+        // Інші процеси також виділяють пам'ять для прийому даних, що будуть розіслані через MPI_Bcast.
+        int *global_image = NULL;
+        int *recvcounts = NULL;
+        int *displs = NULL;
+        if (rank == 0) {
+            global_image = malloc(HEIGHT * WIDTH * sizeof(int));
+            recvcounts = malloc(size * sizeof(int));
+            displs = malloc(size * sizeof(int));
+
+            // Обчислюємо, скільки елементів (пікселів) має приймати кожен процес,
+            // а також зсув (displacement) для кожного процесу в глобальному масиві.
+            for (int p = 0; p < size; p++) {
+                int p_rows = (p < remainder) ? (rows_per_proc + 1) : rows_per_proc;
+                recvcounts[p] = p_rows * WIDTH;
+                displs[p] = (p == 0) ? 0 : displs[p - 1] + recvcounts[p - 1];
+            }
+        } else {
+            // На процесах, що не є root, також виділяємо пам'ять для global_image,
+            // щоб отримати результат розсилки через MPI_Bcast.
+            global_image = malloc(HEIGHT * WIDTH * sizeof(int));
+        }
+
+        // Збір локальних результатів у глобальний масив на процесі 0.
+        // MPI_Gatherv дозволяє збирати змінну кількість елементів від кожного процесу.
+        // На non-root процесах send buffer (local_image) надсилається, а на root він приймається і зберігається в global_image.
+        MPI_Gatherv(local_image, local_rows * WIDTH, MPI_INT,
+                    global_image, recvcounts, displs, MPI_INT,
+                    0, MPI_COMM_WORLD);
+
+        // NEW: Запис зображення поточного кадру у форматі PPM (тільки на процесі 0).
+        if (rank == 0) {
+            printf("\nMPI: Множина Мандельброта\n");
+            printf("Глобальна контрольна сума (сума ітерацій): %lld\n", global_total);
+            printf("Глобально мінімальна кількість ітерацій: %d\n", global_min);
+            printf("Глобально максимальна кількість ітерацій: %d\n", global_max);
+            printf("Час виконання: %f секунд\n", global_time);
+            // Вивід зразкових значень пікселів (кутів та центру)
+            printf("Верхній лівий піксель: %d\n", global_image[0]);
+            printf("Верхній правий піксель: %d\n", global_image[WIDTH - 1]);
+            printf("Нижній лівий піксель: %d\n", global_image[(HEIGHT - 1) * WIDTH]);
+            printf("Нижній правий піксель: %d\n", global_image[HEIGHT * WIDTH - 1]);
+            printf("Центр: %d\n", global_image[(HEIGHT / 2) * WIDTH + (WIDTH / 2)]);
+
+            char filename[256];
+            sprintf(filename, "MPI-frame_%04d.ppm", frame);
+            FILE *fp = fopen(filename, "wb");
+            if (fp == NULL) {
+                fprintf(stderr, "Помилка відкриття файлу %s для запису зображення!\n", filename);
+                free(global_image);
+                free(recvcounts);
+                free(displs);
+                MPI_Finalize();
+                return 1;
+            }
+            // Запис заголовку файлу PPM: формат P6, розміри зображення та максимальне значення кольору (255)
+            fprintf(fp, "P6\n%d %d\n255\n", WIDTH, HEIGHT);
+            // Перетворення значень ітерацій у відтінки сірого та запис пікселів у файл
+            for (int i = 0; i < HEIGHT; i++) {
+                for (int j = 0; j < WIDTH; j++) {
+                    int iter = global_image[i * WIDTH + j];
+                    unsigned char color = (unsigned char)(255 * iter / MAX_ITER);
+                    fwrite(&color, 1, 1, fp);
+                    fwrite(&color, 1, 1, fp);
+                    fwrite(&color, 1, 1, fp);
+                }
+            }
+            fclose(fp);
+            // Вивід додаткової інформації про кадр
+            printf("Frame %d: Глобальна контрольна сума = %lld, мін = %d, макс = %d, час = %f секунд\n",
+                   frame, global_total, global_min, global_max, global_time);
+        }
+
+        // Звільнення пам'яті для поточного кадру.
+        free(local_image);
+        free(global_image);
+        if (rank == 0) {
+            free(recvcounts);
+            free(displs);
         }
     }
 
-
-    long long global_total;
-    int global_min, global_max;
-    double global_time;
-
-    // Збір глобальної статистики за допомогою MPI_Reduce.
-    // MPI_Reduce виконує операцію редукції (сума, мінімум, максимум) над значеннями з усіх процесів.
-    // Результат операції надсилається до процесу, заданого як root (тут 0).
-    MPI_Reduce(&local_total, &global_total, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-    MPI_Reduce(&local_min, &global_min, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
-    MPI_Reduce(&local_max, &global_max, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
-
-    // Фіксуємо час завершення обчислень разом з кінцем передачі усіх даних на 0 процес
-    double end_time = MPI_Wtime();
-    double local_time = end_time - start_time;
-
-    // Для часу обчислень беремо максимальне значення серед усіх процесів,
-    // що дає нам загальний час виконання, оскільки деякі процеси можуть працювати довше.
-    MPI_Reduce(&local_time, &global_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-
-    // Підготовка до збору глобального зображення.
-    // Процес 0 (root) виділяє пам'ять для отримання результатів з усіх процесів.
-    // Інші процеси також виділяють пам'ять для прийому даних, що будуть розіслані через MPI_Bcast.
-    int *global_image = NULL;
-    int *recvcounts = NULL;
-    int *displs = NULL;
+    // NEW: Після генерації всіх кадрів об'єднання їх у анімований GIF за допомогою ImageMagick.
     if (rank == 0) {
-        global_image = malloc(HEIGHT * WIDTH * sizeof(int));
-        recvcounts = malloc(size * sizeof(int));
-        displs = malloc(size * sizeof(int));
-
-        // Обчислюємо, скільки елементів (пікселів) має приймати кожен процес,
-        // а також зсув (displacement) для кожного процесу в глобальному масиві.
-        for (int p = 0; p < size; p++) {
-            int p_rows = (p < remainder) ? (rows_per_proc + 1) : rows_per_proc;
-            recvcounts[p] = p_rows * WIDTH;
-            displs[p] = (p == 0) ? 0 : displs[p - 1] + recvcounts[p - 1];
-        }
-    } else {
-        // На процесах, що не є root, також виділяємо пам'ять для global_image,
-        // щоб отримати результат розсилки через MPI_Bcast.
-        global_image = malloc(HEIGHT * WIDTH * sizeof(int));
-    }
-
-    // Збір локальних результатів у глобальний масив на процесі 0.
-    // MPI_Gatherv дозволяє збирати змінну кількість елементів від кожного процесу.
-    // На non-root процесах send buffer (local_image) надсилається, а на root він приймається і зберігається в global_image.
-    MPI_Gatherv(local_image, local_rows * WIDTH, MPI_INT,
-                global_image, recvcounts, displs, MPI_INT,
-                0, MPI_COMM_WORLD);
-
-    // Лише процес 0 виводить підсумкову інформацію та статистику обчислень.
-    if (rank == 0) {
-        printf("\nMPI: Множина Мандельброта\n");
-        printf("Глобальна контрольна сума (сума ітерацій): %lld\n", global_total);
-        printf("Глобально мінімальна кількість ітерацій: %d\n", global_min);
-        printf("Глобально максимальна кількість ітерацій: %d\n", global_max);
-        printf("Час виконання: %f секунд\n", global_time);
-        // Вивід зразкових значень пікселів (кутів та центру)
-        printf("Верхній лівий піксель: %d\n", global_image[0]);
-        printf("Верхній правий піксель: %d\n", global_image[WIDTH - 1]);
-        printf("Нижній лівий піксель: %d\n", global_image[(HEIGHT - 1) * WIDTH]);
-        printf("Нижній правий піксель: %d\n", global_image[HEIGHT * WIDTH - 1]);
-        printf("Центр: %d\n", global_image[(HEIGHT / 2) * WIDTH + (WIDTH / 2)]);
-    }
-
-    // Звільнення виділеної пам'яті.
-    free(local_image);
-    free(global_image);
-    if (rank == 0) {
-        free(recvcounts);
-        free(displs);
+        system("convert -delay 10 -loop 0 MPI-frame_*.ppm MPI-animation.gif");
+        printf("Animated GIF generated as MPI-animation.gif\n");
     }
 
     // Завершення роботи MPI-середовища.
